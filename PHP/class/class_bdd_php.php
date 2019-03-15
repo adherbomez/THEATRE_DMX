60083d0d125a8f79932533eb391e583e68141499
mlepelletier <mlepelletier@la-providence>
2019-03-15 19:25:43 +0100

    php 15/03

--- PHP/class/class_bdd_php.php
+++ PHP/class/class_bdd_php.php
@@ -5,7 +5,7 @@ PROJET THEATRE DMX512
 DEVELOPPÉ PAR : MAXIME LEPELLETIER 
 BTS SN2
 
-		- classe BDD de l'ihm php - 
+		- classe de gestion de l'ihm php - 
 
 cette classe permet de se connecter / interagir / se deconnecter de la base de donnée de l'application
 pour pouvoir ajouter et supprimer des scenes ou des programmes , sans avoir a passer par le c++ et le protocole tcp-ip
@@ -13,31 +13,32 @@ pour pouvoir ajouter et supprimer des scenes ou des programmes , sans avoir a pa
 /----------------------------------------------------------------*/
 
 Class BDD{
-
-		private $bdd ;						// variable qui contiendra la connexion a la bdd
-		private $req;						//fonction qui permet de se connecter a la base de donnée de l'application.
+		
+		private $_Bdd ;										// variable qui contiendra la connexion a la bdd
+		private $_Req;										//fonction qui permet de se connecter a la base de donnée de l'application.
 
 //permet d'établir un deconnexion entre l'ihm et la base de donnée
 public function connexion()
 {
 	try 
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+		$_Bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
 	} 
-	catch (PDOException $e) 
+	catch (PDOException $e) 								//gestion de l'erreur de connexion a la base de donnée
 	{
-   		print "Erreur !: " . $e->getMessage() . "<br/>";
-  		die();
+			print "Erreur !: " . $e->getMessage() . "<br/>";
+			die();
 	}
 }
 
 //permet la deconnexion de la base de donnee
 public function deconnexion()
 {
-	$bdd = NULL;
+	$bdd = NULL;											
 }
 
-
-
-		
+// permet l'envoi d'une requete a la base de donnée 
+public function envoyer($_Req)
+{
+	return true;
 }
--- PHP/class/class_progmanager.php
+++ PHP/class/class_progmanager.php
@@ -1,5 +1,9 @@
 <?php
-require('class_bdd_php.php');/*----------------------------------------------------------------/
+require('class_bdd_php.php');
+require('class_programme.php');
+require('class_socket.php');
+
+/*----------------------------------------------------------------/
 PROJET THEATRE DMX512
 DEVELOPPÉ PAR : MAXIME LEPELLETIER 
 BTS SN2
@@ -10,35 +14,74 @@ cette classe permet de parametrer les scene de lumiere.
 ces derniers composent les programmes (objet du fichier class_programme.php)
 /----------------------------------------------------------------*/
 
-	Class progmanager extends Programme
-
+	Class progmanager 
 	{ 
 	
-		protected $_Programme;
+	$programme = new Programme();
 
 // fonction qui permet d'ajouter un Programme sur l'ihm
-		public function AjouterProgramme()
-		{
-			
+		public function AjouterProg()
+		{	
+			if(1)
+			{
+				connexion();					
+				envoyer();
+				deconnexion();					
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+				
 		}
+
 // fonction qui permet de modifier un Programme
-		public function ModifierProgramme()
+		public function ModifierProg()
 		{
-			
+			if(1)
+			{
+				connexion();
+				envoyer();
+				deconnexion();	
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 // fonction qui permet de supprimer un Programme sur l'ihm
-		public function SupprimerProgramme()
+		public function SupprimerProg()
 		{
-
+			if(1)
+			{
+				connexion();
+				envoyer();
+				deconnexion();	
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
+
 // fonction qui permet de lancer un Programme a partir de l'ihm		
-		public function RunProgramme()
+		public function RunProg()
 		{
-
-			
+			if(1)
+			{
+				connexion();
+				envoyer();
+				deconnexion();	
+				return true;
+			}
+			else
+			{
+				return false;
+			}	
 		}
-
-
 	}
 
 ?>
